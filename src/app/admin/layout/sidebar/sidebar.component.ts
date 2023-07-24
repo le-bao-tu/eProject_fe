e3a96a6e6130453d549b98e99040b19410bbca43
DESKTOP-O9ILU3T\LeBaoTu <lebaotu05122002@gmail.com>
2023-07-24 17:11:32 +0700

    lập trình đẩy code mới lên

--- src/app/admin/layout/sidebar/sidebar.component.ts
+++ src/app/admin/layout/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { AccountService } from 'src/app/service/account.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,8 +8,9 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  email:any;
   userName:any;
-  constructor(private jwtHelper: JwtHelperService) { }
+  constructor(private jwtHelper: JwtHelperService, private accountService:AccountService ) { }
 
   ngOnInit() {
     this.GetTokenByName();
@@ -18,7 +20,13 @@ export class SidebarComponent implements OnInit {
     var token = localStorage.getItem('currentUser')
     const tokenDecode = this.jwtHelper.decodeToken(token);
     if(tokenDecode != null) {
-      this.userName = tokenDecode.sub[0].Value;
+      this.email = tokenDecode["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"];
+      this.accountService.GetByNameToken(this.email).subscribe((res:any) => {
+        console.log(res);
+        if(res.code == 200) {
+           this.userName = res.data.userName
+        }
+      })
     }
   }
 }
--- src/app/service/account.service.ts
+++ src/app/service/account.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Account } from '../models/account.model';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 const urlLogin = "https://localhost:44328/api/user/login";
+const urlgetByNameTokenLogin = "https://localhost:44328/api/user/get-by-name-token";
 const urlGetListUser = "https://localhost:44337/api/HNI/getlist-user"
 
 @Injectable({
@@ -18,5 +20,12 @@ export class AccountService {
 
   GetListAccount(data:Object):Observable<Object> {
     return this.http.post<Object>(urlGetListUser,data)
+  }
+
+  GetByNameToken(email:string):Observable<Object> {
+    return this.http.get<Object>(urlgetByNameTokenLogin+`?email=${email}`).pipe(
+         tap(() => console.log("OK"),
+      )
+    )
   }
 }
